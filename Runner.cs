16aa60ba31b1bc33093b39a5b9396b1be76bbca7
Alexey <immediant@gmail.com>
2023-09-11 14:10:35 +0300

    Add files via upload

---
+++ Runner.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetParmsToApp
+{
+    public class Runner : BaseVM
+    {
+        private string parm;
+
+        public string Parm { get => parm; set => SetProperty(ref parm, value); }
+
+        private Command run;
+
+        public Command Run => run ?? (run = new Command(RunCom, () => true));
+
+        private void RunCom()
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = @"D:\Alex\StartWithParms\bin\Debug\StartWithParms.exe";
+            process.StartInfo.Arguments = Parm;
+            process.Start();
+        }
+    }
+}
